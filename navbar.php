52d51bfbdb39937dc3537499b59acbf095e195df
elJulioDev <alexisdevelopgz@gmail.com>
2026-04-03 21:16:50 -0300

    Se añadio icono en las pestañas.

--- navbar.php
+++ navbar.php
@@ -8,6 +8,15 @@ $nombreUsuario = isset($_SESSION['usuario_nombre']) ? htmlspecialchars($_SESSION
 // Obtener el nombre del archivo actual para marcar el enlace activo
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
+<script>
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/svg+xml';
+    link.rel = 'icon';
+    link.href = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%232563eb'><path fill-rule='evenodd' d='M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z' clip-rule='evenodd' /></svg>";
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
+</script>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;600;700&display=swap" rel="stylesheet">
 <style>
