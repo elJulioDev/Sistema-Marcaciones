4eb3bf958132691bbf6404c3550012ee21ca0d96
elJulioDev <alexisdevelopgz@gmail.com>
2026-04-05 20:46:34 -0400

    Se corrigio texto del panel.

--- panel.php
+++ panel.php
@@ -215,7 +215,7 @@ $nombreUsuario = isset($_SESSION['usuario_nombre']) ? htmlspecialchars($_SESSION
                     </div>
                     <div class="card-info">
                         <h2>Calendario Visual</h2>
-                        <p>Estado de marcaciones diarias o semanales.</p>
+                        <p>Estado de marcaciones diarias, semanales y mensuales.</p>
                     </div>
                     <div class="card-arrow">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"/></svg>
