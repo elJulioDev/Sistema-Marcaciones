4eb4209f68514dbcc5a336ef8c2edbf58d050e9f
elJulioDev <alexisdevelopgz@gmail.com>
2026-08-09 23:58:58 -0400

    Corregir enlace del panel a /importar (apuntaba al archivo plano eliminado)

--- app/Views/panel/index.php
+++ app/Views/panel/index.php
@@ -17,7 +17,7 @@
         </span>
     </a>
 
-    <a href="<?= base_url('/importar_marcaciones.php') ?>" class="panel-card panel-importar">
+    <a href="<?= base_url('/importar') ?>" class="panel-card panel-importar">
         <span class="panel-icon">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
         </span>
